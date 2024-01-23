91a04ae0322303e4a7a4cd7e958619e41c8e8829
Panther1986 <144061903+Panther1986@users.noreply.github.com>
2024-01-23 09:34:19 +0000

    Deploying to gh-pages from @ Panther1986/goit-js-hw-10@6693eeea382ea96346ae87350a8daad6b9f6018f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import{f as t}from"./assets/vendor-992cd48f.js";const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",{options:o});
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{f as o}from"./assets/vendor-992cd48f.js";let t;o("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),t=e[0]}});console.log(t);
 //# sourceMappingURL=commonHelpers.js.map
